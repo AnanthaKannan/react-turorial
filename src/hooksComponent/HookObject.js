6e78ce6ceb1ffc53cef9b9f3ce137e3c0ea3c218
SreeAnanthaKannnan <Sree.kannan@rapidqube.com>
2019-10-19 16:07:34 +0530

    add hook object

---
+++ src/hooksComponent/HookObject.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const HookObject = () => {
+
+    const [ name, setName ] = useState({firstName:'', lastName:''});
+
+    const handleFirstName = (e) =>{
+        setName({...name, firstName:e.target.value})
+    }
+
+    const handleLastName = (e) =>{
+        setName({ ...name, lastName: e.target.value })
+    }
+ 
+    return (
+        <div>
+          <input onChange={ handleFirstName } value={ name.firstName } />
+          <input onChange={ handleLastName } value={ name.lastName } />
+          <h2> { JSON.stringify(name)} </h2>
+        </div>
+    )
+}
+
+export default HookObject
