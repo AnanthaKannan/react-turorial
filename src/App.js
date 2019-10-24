249901130654a9d883c4b6e0490d0044c89dee9b
SreeAnanthaKannan <sreeananthakannan@gmail.com>
2019-10-24 23:21:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { useReducer } from 'react';
 import './App.css';
-import ClassClick from './component/ClassClick';
-import FunctionClick from './component/FunctionClick';
-import EventBinding from './component/EventBinding';
-import ParentComponent from './component/ParentComponent';
-import ConditionalRendering from './component/ConditionalRendering';
-import ListRendering from './component/ListRendering';
-import Styling from './styling/Styling';
-import Form from './component/Form';
-import ConditionalyRun from './hooksComponent/ConditionalyRun';
-import MouseMove from './hooksComponent/MouseMove';
-import ConponentUnmount from './hooksComponent/ConponentUnmount';
+import Websoket from './websoket/websoket';
+// import ClassClick from './component/ClassClick';
+// import FunctionClick from './component/FunctionClick';
+// import EventBinding from './component/EventBinding';
+// import ParentComponent from './component/ParentComponent';
+// import ConditionalRendering from './component/ConditionalRendering';
+// import ListRendering from './component/ListRendering';
+// import Styling from './styling/Styling';
+// import Form from './component/Form';
+// import ConditionalyRun from './hooksComponent/ConditionalyRun';
+// import MouseMove from './hooksComponent/MouseMove';
+// import ConponentUnmount from './hooksComponent/ConponentUnmount';
 
-import HookCounter from './hooksComponent/HookCounter';
-import HookObject from './hooksComponent/HookObject';
-import HookArray from './hooksComponent/HookArray';
-import FetchData from './hooksComponent/FetchData';
-import FetchDataButton from './hooksComponent/FetchDataButton';
-import Context from './hooksComponent/context/Context';
-import Reducer from './hooksComponent/Reducer';
-import ReducerTwo from './hooksComponent/ReducerTwo';
-import GlobalState from './hooksComponent/globalState/GlobalState';
-import GlobalStateA from './hooksComponent/globalState/GlobalStateA';
-import GlobalStateB from './hooksComponent/globalState/GlobalStateB';
+// import HookCounter from './hooksComponent/HookCounter';
+// import HookObject from './hooksComponent/HookObject';
+// import HookArray from './hooksComponent/HookArray';
+// import FetchData from './hooksComponent/FetchData';
+// import FetchDataButton from './hooksComponent/FetchDataButton';
+// import Context from './hooksComponent/context/Context';
+// import Reducer from './hooksComponent/Reducer';
+// import ReducerTwo from './hooksComponent/ReducerTwo';
+// import GlobalState from './hooksComponent/globalState/GlobalState';
+// import GlobalStateA from './hooksComponent/globalState/GlobalStateA';
+// import GlobalStateB from './hooksComponent/globalState/GlobalStateB';
 
 export const UserContext = React.createContext();
 export const ChannelContext = React.createContext();
@@ -42,7 +43,9 @@ function App() {
 
   return (
     <div className="App" >
-    <ConponentUnmount />
+
+    <Websoket />
+    {/* <ConponentUnmount /> */}
     {/* <MouseMove /> */}
     {/* <ConditionalyRun /> */}
       {/* <Form /> */}
@@ -56,11 +59,12 @@ function App() {
       {/* <HookCounter /> */}
       {/* <HookCounterThree /> */}
 
-      <CountContext.Provider value={{ countState: count, countDispatch: dispatch }} >
+      {/* <CountContext.Provider value={{ countState: count, countDispatch: dispatch }} >
     <GlobalState />
     <GlobalStateA />
     <GlobalStateB />
-    </CountContext.Provider>
+    </CountContext.Provider> */}
+
       {/* <UserContext.Provider value={'kannan'} age={24}>
           <ChannelContext.Provider value={'rapid'} >
           <Context />
