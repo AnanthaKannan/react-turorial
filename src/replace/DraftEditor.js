7e0ca9503b20b2686f399ccc4142c0d6890f746d
SreeAnanthaKannnan <Sree.kannan@rapidqube.com>
2019-11-16 16:32:02 +0530

    'added'

--- src/replace/DraftEditor.js
+++ src/replace/DraftEditor.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { EditorState, Editor, CompositeDecorator  } from 'draft-js';
+import React, { Component } from 'react';
+import { EditorState, Editor, CompositeDecorator, SelectionState, Modifier } from 'draft-js';
 import './draft.css'
 
 export default class DraftEditor extends Component {
@@ -10,14 +10,17 @@ export default class DraftEditor extends Component {
           search: '',
           replace: '',
           editorState: EditorState.createEmpty(),
+          show:false,
+          x:0,
+          y:0,
+          suggesion:[],
+          ignore:null
         }
       }
 
-    //   onChangeSearch = (e) => {
-    //     this.setState({
-    //       search: e.target.value,
-    //     });
-    //   }
+    componentDidMount(){
+      this.fatalResponse();
+    }
 
     onChangeSearch = (e) => {
         const search = e.target.value;
@@ -32,11 +35,72 @@ export default class DraftEditor extends Component {
           replace: e.target.value,
         });
       }
-    
-      onReplace = () => {
-        console.log(`replacing "${this.state.search}" with "${this.state.replace}"`);
+
+      fatalResponse = () =>{
+        let result = [{
+          fatal: false,
+          actual: 'Ths',
+          expected:
+           [ 'ohs', "T'S", "TH'S", 'Tbs','Th','The','This','Tho',
+             'Thu', 'Thus','Thy','Ts','DHS','HHS','HS','HTS','THC', 'VHS'
+          ] },
+          {
+            fatal: false,
+            actual: 'some',
+            expected:
+             [ 'same', 'soft', 'added'] }];
+        return result;
       }
     
+      onReplace = (blockKey, start, end, suggestWord) => {
+
+        const { editorState } = this.state;
+        const selectionsToReplace = [];
+            const blockSelection = SelectionState
+              .createEmpty(blockKey)
+              .merge({
+                anchorOffset: start,
+                focusOffset: end,
+              });
+      
+        selectionsToReplace.push(blockSelection)
+        
+        let contentState = editorState.getCurrentContent();
+      
+        selectionsToReplace.forEach(selectionState => {
+          contentState = Modifier.replaceText(
+            contentState,
+            selectionState,
+            suggestWord,
+          )
+        });
+      
+        this.setState({
+          editorState: EditorState.push(
+            editorState,
+            contentState,
+          )
+        })
+      }
+
+      mouseEnter = (blockKey, start, end, suggesion, element) =>{
+        console.log(blockKey, start, end, suggesion);
+        const x = element.clientX;
+        const y = element.pageY;
+        let suggest = [];
+        suggesion.map((SuggestWord) =>{
+          suggest.push(<span key={SuggestWord} onClick={() => this.onReplace(blockKey, start, end, SuggestWord)} className='p-2 m-0 select rounded-top'> { SuggestWord } </span>)
+        })
+        const ignore = <p onClick={()=> this.ignore_(start, end, 'text')} className='p-2 m-0 ignore rounded-bottom'><i className="fa fa-trash-o" aria-hidden="true"></i>ignore</p>
+        this.setState({show: true, x, y, suggesion:suggest, ignore})
+      }
+
+      ignore_ = (start, end, text) =>{
+        this.setState({
+          editorState: EditorState.set(this.state.editorState, { decorator: this.generateDecorator(text) }),
+        });
+      }
+
       onChange = (editorState) => {
         this.setState({
           editorState,
@@ -44,26 +108,37 @@ export default class DraftEditor extends Component {
       }
 
        generateDecorator = (highlightTerm) => {
-        // const regex = new RegExp(`\b(some)\b`, 'g');
-        // const pattern = /\b(some)\b/g;
+        let suggesion_res = this.fatalResponse();
         const pattern = `\\b(${highlightTerm})\\b`;
         const regex = new RegExp(pattern,"g");
         return new CompositeDecorator([{
           strategy: (contentBlock, callback) => {
-            //   console.log(contentBlock)
             if (highlightTerm !== '') {
               this.findWithRegex(regex, contentBlock, callback);
             }
           },
-          component: this.SearchHighlight,
+          component: (props) => this.SearchHighlight(props, suggesion_res),
         }])
       };
+
+      SearchHighlight = (props, suggesion_res) => {
+        console.log("props", props)
+        let suggest = [];
+        const text = props.children[0].props.text;
+        let actual = suggesion_res.filter((obj) => obj.actual.toLowerCase() == text.toLowerCase());
+        if(actual.length > 0){
+          suggest = actual[0].expected;
+        }
+        console.log('actual', actual, 'text', text);
+        return ( <span onMouseEnter={(e)=>this.mouseEnter(props.blockKey, props.start, props.end, suggest, e)} className="error">{props.children}
+        </span>);
+      };
+
 
        findWithRegex = (regex, contentBlock, callback) => {
         const text = contentBlock.getText();
         console.log(text)
         let matchArr, start, end;
-        // console.log(regex.exec(text));
         while ((matchArr = regex.exec(text)) !== null) {
           start = matchArr.index;
           end = start + matchArr[0].length;
@@ -71,9 +146,9 @@ export default class DraftEditor extends Component {
         }
       };
 
-       SearchHighlight = (props) => ( <span onClick={()=> alert()} className="error">{props.children}</span>);
 
     render() {
+      const { show, x, y, suggesion, ignore } = this.state;
         return (
             <div className="container">
                 <h3>daraftjs</h3>
@@ -98,6 +173,15 @@ export default class DraftEditor extends Component {
             Replace
           </button>
         </div>
+
+        {
+                    show && 
+                <div className="suggest rounded" style={{left: x, top:y }} onMouseLeave={() => this.setState({show:false})}>
+                    { suggesion }
+                    { ignore }
+                    {/* <p className='p-2 m-0 ignore rounded-bottom'><i className="fa fa-trash-o" aria-hidden="true"></i>ignore</p> */}
+                </div>
+               }
       </div>
             </div>
         )
